3fec366a7de4f8372354d44ffc9a058009cfd0d0
Edgar Talledos <onimenotsuki@gmail.com>
2020-07-24 13:10:45 -0500

    Primeros pasos

--- server.js
+++ server.js
@@ -4,7 +4,19 @@ const Telegraf = require('telegraf');
 const dotenv = require('dotenv');
 dotenv.config();
 
+const token = process.env.TELEGRAM_TOKEN;
 const port = process.env.PORT || 8000;
+
+const bot = new Telegraf(token);
+
+app.use(bot.webhookCallback('/ruta-bot'));
+
+bot.telegram.setWebhook('https://85f0dad635a5.ngrok.io/ruta-bot');
+bot.command('/test', (ctx) => ctx.reply('Hola mundo, Bot'));
+
+app.post('/ruta-bot', (req, res) => {
+  res.send('Llamada a la ruta del bot');
+});
 
 app.get('/', (_, res) => res.send('Hola mundo'));
 
